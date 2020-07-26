f1c4a230061bef578f33395a0d297f7ba97992e7
wisungyo <wisungyo@gmail.com>
2020-07-26 11:53:58 +0700

    minor

--- app/src/main/java/com/binar/rockpaperscissors/MainActivity.kt
+++ app/src/main/java/com/binar/rockpaperscissors/MainActivity.kt
@@ -5,7 +5,6 @@ import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.os.Vibrator
 import android.util.Log
 import android.view.View
